00e115e6a403f688c4d49943687845c34f7a00e1
Gustavs Venters <gustavs.venters@gmail.com>
2020-04-20 00:35:37 +0200

    Add Sentry connection

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,8 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ErrorHandler, Injectable, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://fe340712f79f444299cdcd4f9b49966f@o380290.ingest.sentry.io/5205952"
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    // no need to show report dialog :)
+    // Sentry.showReportDialog({ eventId });
+    alert('Ooops, an error...')
+  }
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +29,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
